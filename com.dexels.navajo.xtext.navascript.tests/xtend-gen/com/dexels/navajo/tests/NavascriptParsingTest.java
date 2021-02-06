985e4bbad4a8e5b3d29c1ce589cf475cdfbb6ed4
Arjen Schoneveld <aschoneveld@dexels.com>
2021-02-06 12:13:10 +0100

    changes

--- com.dexels.navajo.xtext.navascript.tests/xtend-gen/com/dexels/navajo/tests/NavascriptParsingTest.java
+++ com.dexels.navajo.xtext.navascript.tests/xtend-gen/com/dexels/navajo/tests/NavascriptParsingTest.java
@@ -5,23 +5,43 @@ package com.dexels.navajo.tests;
 
 import com.dexels.navajo.navascript.Navascript;
 import com.dexels.navajo.tests.NavascriptInjectorProvider;
+import com.google.inject.Inject;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.testing.InjectWith;
+import org.eclipse.xtext.testing.extensions.InjectionExtension;
+import org.eclipse.xtext.testing.util.ParseHelper;
+import org.eclipse.xtext.xbase.lib.Exceptions;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-/* @ExtendWith(/* name is null */)
-@InjectWith(NavascriptInjectorProvider.class) */@SuppressWarnings("all")
+@ExtendWith(InjectionExtension.class)
+@InjectWith(NavascriptInjectorProvider.class)
+@SuppressWarnings("all")
 public class NavascriptParsingTest {
-  /* @Inject
-   */private /* ParseHelper<Navascript> */Object parseHelper;
+  @Inject
+  private ParseHelper<Navascript> parseHelper;
   
   @Test
   public void loadModel() {
-    throw new Error("Unresolved compilation problems:"
-      + "\nThe field NavascriptParsingTest.parseHelper refers to the missing type ParseHelper"
-      + "\nparse cannot be resolved"
-      + "\neResource cannot be resolved"
-      + "\nerrors cannot be resolved"
-      + "\nisEmpty cannot be resolved"
-      + "\njoin cannot be resolved");
+    try {
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append("Hello Xtext!");
+      _builder.newLine();
+      final Navascript result = this.parseHelper.parse(_builder);
+      Assertions.assertNotNull(result);
+      final EList<Resource.Diagnostic> errors = result.eResource().getErrors();
+      boolean _isEmpty = errors.isEmpty();
+      StringConcatenation _builder_1 = new StringConcatenation();
+      _builder_1.append("Unexpected errors: ");
+      String _join = IterableExtensions.join(errors, ", ");
+      _builder_1.append(_join);
+      Assertions.assertTrue(_isEmpty, _builder_1.toString());
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
 }
--- com.dexels.navajo.xtext.navascript/xtend-gen/com/dexels/navajo/generator/NavascriptGenerator.java
+++ com.dexels.navajo.xtext.navascript/xtend-gen/com/dexels/navajo/generator/NavascriptGenerator.java
@@ -3,14 +3,19 @@
  */
 package com.dexels.navajo.generator;
 
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.generator.AbstractGenerator;
+import org.eclipse.xtext.generator.IFileSystemAccess2;
+import org.eclipse.xtext.generator.IGeneratorContext;
+
 /**
  * Generates code from your model files on save.
  * 
  * See https://www.eclipse.org/Xtext/documentation/303_runtime_concepts.html#code-generation
  */
 @SuppressWarnings("all")
-public class NavascriptGenerator /* implements AbstractGenerator  */{
+public class NavascriptGenerator extends AbstractGenerator {
   @Override
-  public void doGenerate(final /* Resource */Object resource, final /* IFileSystemAccess2 */Object fsa, final /* IGeneratorContext */Object context) {
+  public void doGenerate(final Resource resource, final IFileSystemAccess2 fsa, final IGeneratorContext context) {
   }
 }
